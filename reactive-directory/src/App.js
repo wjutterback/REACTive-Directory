0898c26f324cb8117701ed75d0603390fef07bb8
Will Utterback <uwill08@gmail.com>
2021-04-23 23:59:06 -0500

    Created react app, nav/table components

---
+++ reactive-directory/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavHeader from './components/nav';
+import Table from './components/table';
+
+function App() {
+  return (
+    <div>
+      <NavHeader />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactive-directory/src/components/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function navHeader() {
+  return (
+    <div className='container'>
+      <div className='jumbotron '>
+        <h1>Employee Directory</h1>
+      </div>
+    </div>
+  );
+}
+
+export default navHeader;
